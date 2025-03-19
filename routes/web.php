5dc6a6cf27971afb62d2ff45db11d13b8d80a8da
Tidragon <theomoon77@gmail.com>
2025-03-19 11:42:35 +0100

    style of login / out home ,...

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@ Route::get('/login', [UserController::class, 'showLogin'])->name('login');
 Route::post('/login', [UserController::class, 'login'])->name('login.submit');
 Route::get('/register', [UserController::class, 'showRegister'])->name('register');
 Route::post('/register', [UserController::class, 'register'])->name('register.submit');
+Route::post('/profile/upload', [UserController::class, 'uploadProfilePicture'])->name('profile.upload');
  
 // Protected routes
 Route::middleware('auth')->group(function() {
+    Route::get('/profile/edit', [UserController::class, 'edit'])->name('auth.edit');
+    Route::post('/profile/edit', [UserController::class, 'profileupdate'])->name('auth.update');
     Route::get('/profile', [UserController::class, 'profile'])->name('profile');
     Route::get('/logout', [UserController::class, 'webLogout'])->name('logout');
 });
